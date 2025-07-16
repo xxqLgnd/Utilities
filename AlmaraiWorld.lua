f84d02490b0737a1bc8061d51d3626bdc24c230a
xxqLgnd <yvangengeryt@mail.ru>
2025-07-16 19:17:38 +0300

    Delete

--- AlmaraiWorld.lua
+++
@@ -1,6 +0,0 @@
---xxqLgnd
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-for _,v in ipairs({"Milk", "Strawberry", "Apple", "Raspberry"}) do
-    ReplicatedStorage.BridgeNet2.dataRemoteEvent:FireServer({{ReplicatedStorage.Truck_Fruit_Strawberry_Nivel03, 50, 10000, "Milk"}, utf8.char(5)})
-end
